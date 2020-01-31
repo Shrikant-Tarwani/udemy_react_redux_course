5ecee1ed0a51dd0d98586cb092ab0c99bfd2b33a
shrikant_t <shrikant.t@lucideustech.com>
2020-01-31 17:55:11 +0530

    Show custom children

--- components/src/index.js
+++ components/src/index.js
@@ -7,10 +7,15 @@ import ApprovalCard from "./ApprovalCard"
 const App = () => {
     return (
         <div className="ui container comments">
-            <ApprovalCard />
+            <ApprovalCard>
             <CommentDetail author="Bhavin" timeAgo = "Today at 4:00 PM" comment = "Nice Blog"  image = {faker.image.avatar()}/>
+            </ApprovalCard>
+            <ApprovalCard>
             <CommentDetail author="Ankur" timeAgo = "Today at 2:00 AM" comment = "Great Blog" image = {faker.image.avatar()}/>
+            </ApprovalCard>
+            <ApprovalCard>
             <CommentDetail author="Kartik" timeAgo = "Yesterday at 4:00 PM" comment = "Good Blog" image = {faker.image.avatar()}/>
+            </ApprovalCard>
         </div>
     );
 }
