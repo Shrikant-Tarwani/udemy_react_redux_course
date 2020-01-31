0c33af153c384b0c433c41313c0b7bc9b2ff7cb4
shrikant_t <shrikant.t@lucideustech.com>
2020-01-31 13:56:52 +0530

    Revert "one comment componenet"
    This reverts commit b331055488044bd94f9cd024cfb2c705e90524ae.

--- components/src/index.js
+++ components/src/index.js
@@ -1,26 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import faker from "faker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-const App = () => {
-    return (
-        <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="Avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="avatar">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 4:00 pm</span>
-                    </div>
-                    <div className="text">Nice Blog!!</div>
-                </div>
-            </div>
-        </div>
-    );
-}
+ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
