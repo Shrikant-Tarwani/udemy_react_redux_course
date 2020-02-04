c81510fb413df2e582ef223d0090d66a741863e5
shrikant_t <shrikant.t@lucideustech.com>
2020-02-04 14:38:34 +0530

    Set state of latitude

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -22,15 +22,20 @@ class App extends React.Component{
         super(props);
 
         this.state = {lat : null}
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({lat : position.coords.latitude})
+            },
+            err => console.log(err)
+        );
     }
 
     render(){
-        window.navigator.geolocation.getCurrentPosition(
-                position => console.log(position),
-                err => console.log(err)
-            );
-            return(
-                <div>l</div>
+       
+            // console.log(this.state.lat);
+            return(                
+                <div>latitude: {this.state.lat}</div>
             )
     }
 }
