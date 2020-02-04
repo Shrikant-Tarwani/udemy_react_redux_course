bbf461d660d27698b5fe6855196c2354f7045eba
shrikant_t <shrikant.t@lucideustech.com>
2020-02-04 13:30:34 +0530

    Add constructor and initialize state

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 
-const App = () => {
 
-    window.navigator.geolocation.getCurrentPosition(
-        position => console.log(position),
-        err => console.log(err)
-    );
-    return(
-        <div></div>
-    )
+//Functional component
+
+// const App = () => {
+
+//     window.navigator.geolocation.getCurrentPosition(
+//         position => console.log(position),
+//         err => console.log(err)
+//     );
+//     return(
+//         <div></div>
+//     )
+// }
+
+//Class Component
+
+class App extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {lat : null}
+    }
+
+    render(){
+        window.navigator.geolocation.getCurrentPosition(
+                position => console.log(position),
+                err => console.log(err)
+            );
+            return(
+                <div>l</div>
+            )
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"));
