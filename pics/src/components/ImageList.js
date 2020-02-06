10adff839d0c7bd34a9d46b9d003d06252348fec
shrikant_t <shrikant.t@lucideustech.com>
2020-02-06 20:37:42 +0530

    Add list of images with key and alt-text

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const ImageList = (props) => {
+    const imgs = props.images.map(({description,id, urls}) => {
+        return(<div key={id}>
+             <img alt={description} src={urls.regular} />
+        </div>
+        )
+    })
     return (
-        <div>Image Search</div>
+        <div>{imgs}</div>
     )
 };
 
