a41d1c4fa2d8cd09fd7376f2bfd80ff50f8ff90f
shrikant_t <shrikant.t@lucideustech.com>
2020-02-06 16:09:02 +0530

    add message to spinner using props

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -48,7 +48,7 @@ class App extends React.Component{
                 if(!this.state.errorMessage && this.state.lat){
                     return <SeasonDisplay lat={this.state.lat} />
                 }
-                return <div><Spinner /></div>
+                return <div><Spinner message="Please allow location" /></div>
             
     }
 }
