46675db7032d56dec6ca806b9224e4e2fbbabe93
shrikant_t <shrikant.t@lucideustech.com>
2020-02-07 15:40:54 +0530

    Add Image Card component and use refs and print height of images

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -1,32 +1,28 @@
 import React from 'react';
 
-class ImageCard extends React.Component{
-    constructor(props){
-        super(props);
-        this.imageRef = React.createRef();
-    }
+class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.imageRef = React.createRef();
+  }
 
-    componentDidMount(){
-        this.imageRef.current.addEventListener('load', this.setSpans);
-        
-    }
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
 
-    setSpans = () => {
-            console.log(this.imageRef.clientHeight);
-            
-    }
-    render(){
-        const {description, urls} = this.props.image;
-        return (<div> 
-            <img
-            ref = {this.imageRef} 
-            alt={description} 
-            src = {urls.regular}
-             />
-        </div>);
+  setSpans = () => {
+    console.log(this.imageRef.current.clientHeight)
+  };
 
-    }
+  render() {
+    const { description, urls } = this.props.image;
+
+    return (
+      <div>
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
+      </div>
+    );
+  }
 }
 
 export default ImageCard;
-
