5aac78790216827c54ebb57de91b123e94d1fbec
shrikant_t <shrikant.t@lucideustech.com>
2020-02-10 00:34:28 +0530

    Handle case of no video

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube'
 import VideoList from './VideoList'
+import VideoDetail from './VideoDetail'
 
 class App extends React.Component {
 
@@ -21,7 +22,9 @@ class App extends React.Component {
     }
     
     render() {
-         return (<div className="ui container"> <SearchBar onFormSubmit={this.onTermSubmit}/>
+         return (<div className="ui container"> 
+            <SearchBar onFormSubmit={this.onTermSubmit}/>
+            <VideoDetail video={this.state.selectedVideo}/>
             <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
           </div>)
 
