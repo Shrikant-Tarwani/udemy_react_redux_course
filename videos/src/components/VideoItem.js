e7e8bb151803be15c69842604a76500f1a8a308f
shrikant_t <shrikant.t@lucideustech.com>
2020-02-09 23:56:04 +0530

    Get images from youtube api

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -4,6 +4,7 @@ import React from 'react';
 const VideoItem = ({ video }) => {
   return (
     <div >
+        <img src={video.snippet.thumbnails.default} />
       {video.snippet.title}
     </div>
   );
