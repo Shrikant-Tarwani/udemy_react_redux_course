bcaafc460fa0adc20b1d7594ff0b31be05911063
shrikant_t <shrikant.t@lucideustech.com>
2020-02-09 20:00:27 +0530

    Add app component and search bar component

---
+++ videos/src/components/SearchBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    state = {term :''};
+
+    onInputChange = () => {
+        
+    }    
+    render() {
+         return (<div className="search-bar ui segment">
+                <form className="ui form">
+                    <div className="fiels">
+                        <label>Video Search</label>
+                        <input value={this.state.term} type="text" onChange={this.onInputChange}/>
+                    </div>
+                </form>
+         </div>)
+
+     }
+}
+export default SearchBar;
